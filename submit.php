70c4703f0f3c24f28491b4730bbd50d6914bce88
Peter Fraedrich <peter.fraedrich@coldblue-usa.com>
2015-05-26 16:45:11 -0400

    init commit

---
+++ submit.php
@@ -0,0 +1,36 @@
+<?php
+// check to make sure our POST wasn't empty
+if(isset($_POST)) {
+	// get username from email
+	$x = explode("@",$_POST['uname']);
+	$fname = $x[0];
+	// assign vars
+	$to = $_POST['uname']; // . ',' . 'ishelp@cvent.com';
+	// message body
+	$message =	'New ISHelp On-Demand ticket:' . "\r\n\r\n" . 
+				'Name: ' . $fname . "\r\n" .
+				'E-Mail: ' . $_POST['uname'] . "\r\n" .
+				'Issue: ' . 	$_POST['issue'];
+	// message subject
+	$subject = 'ISHelp On-Demand -- ' . $fname;
+	// set headers
+	$headers = 'From: ' . $_POST['uname'] . "\r\n" .
+				'Reply-To: ' . $_POST['uname'] . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+	// check that our email is sent correctly
+	if (mail($to, $subject, $message, $headers)) {
+		// if message sent, show thank you page
+		header("location: thankyou.html");
+	}
+	else {
+		// if message not sent, show error page
+		header("location: error.html");
+	}
+}
+else {
+	// if POST empty, display error
+	die('no post data to process');
+}
+?>
